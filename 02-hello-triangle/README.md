0242648da5ccc4aee99e24ca6eefcd15c6a8b206
Vu Quang Huy <vquanghuy@gmail.com>
2023-04-09 11:37:51 +0700

    Add HelloTriangle project

---
+++ 02-hello-triangle/README.md
@@ -0,0 +1,24 @@
+# Hello Triangle
+
+Depended libraries
+
+* GLFW
+* GLAD
+
+Generate Xcode project file
+
+```
+cmake -G Xcode -B ./build .
+```
+
+Open project and build
+
+```
+open build/HelloTriangle.xcodeproj/
+```
+
+## TODO
+
+- [ ] Add shader loader
+
+- [ ] Render color triangle
---
+++ 02-hello-triangle/src/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello Triangle", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+    
+    /* Initialize OpenGL context */
+    int version = gladLoadGL((GLADloadfunc) glfwGetProcAddress);
+    if (!version)
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    
+    std::cout << "Loaded OpenGL version: "
+        << GLAD_VERSION_MAJOR(version) << "."
+        << GLAD_VERSION_MINOR(version)
+        << std::endl;
+    
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClearColor(0.16f, 0.24f, 0.32f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
