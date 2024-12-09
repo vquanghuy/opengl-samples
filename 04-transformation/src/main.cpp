b880adbea2157ef33fee339ee52fb3eb056fbfc0
Vu Quang Huy <vquanghuy@gmail.com>
2024-12-09 18:12:55 +0700

    Minor update

--- 04-transformation/src/main.cpp
+++ 04-transformation/src/main.cpp
@@ -11,57 +11,53 @@
 #include "shaderLoader.h"
 #include "textureLoader.h"
 
-glm::mat4 camera(float translate, float rotateZ = 0.0f)
-{
-	glm::mat4 projection = glm::perspective(glm::pi<float>() * 0.25f, 4.0f / 3.0f, 0.1f, 100.f);
-	glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -translate));
-	glm::mat4 model = glm::rotate(glm::mat4(1.0f), rotateZ, glm::vec3(0.0f, 0.0f, 1.0f));
-	return projection * view * model;
+glm::mat4 camera(float translate, float rotateZ = 0.0f) {
+    glm::mat4 projection = glm::perspective(glm::pi<float>() * 0.25f, 4.0f / 3.0f, 0.1f, 100.f);
+    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -translate));
+    glm::mat4 model = glm::rotate(glm::mat4(1.0f), rotateZ, glm::vec3(0.0f, 0.0f, 1.0f));
+    return projection * view * model;
 }
 
-int main(void)
-{
-    GLFWwindow* window;
+int main(void) {
+    GLFWwindow *window;
 
     /* Initialize the library */
     if (!glfwInit())
         return -1;
-    
+
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    
+
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "04 - Transformation", NULL, NULL);
-    if (!window)
-    {
+    if (!window) {
         glfwTerminate();
         return -1;
     }
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
-    
+
     /* Initialize OpenGL context */
     int version = gladLoadGL((GLADloadfunc) glfwGetProcAddress);
-    if (!version)
-    {
+    if (!version) {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
-    
+
     std::cout << "Loaded OpenGL version: "
-        << GLAD_VERSION_MAJOR(version) << "."
-        << GLAD_VERSION_MINOR(version)
-        << std::endl;
+            << GLAD_VERSION_MAJOR(version) << "."
+            << GLAD_VERSION_MINOR(version)
+            << std::endl;
 
     // load shaders
     GLuint shaderProgram = loadShader(
-        "../assets/shaders/rectangle.vert.glsl", 
+        "../assets/shaders/rectangle.vert.glsl",
         "../assets/shaders/rectangle.frag.glsl"
     );
 
@@ -71,14 +67,14 @@ int main(void)
     // setup vertices data (and buffer(s)) and configure vertex attributes
     GLfloat vertices[] = {
         // positions        // texture coords
-        0.5f,  0.5f, 0.0f, 1.0f, 1.0f,   // top right
-        0.5f, -0.5f, 0.0f, 1.0f, 0.0f,   // bottom right
-        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f,   // bottom left
-        -0.5f,  0.5f, 0.0f, 0.0f, 1.0f    // top left 
+        0.5f, 0.5f, 0.0f, 1.0f, 1.0f, // top right
+        0.5f, -0.5f, 0.0f, 1.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, // bottom left
+        -0.5f, 0.5f, 0.0f, 0.0f, 1.0f // top left
     };
-    GLuint indices[] = {  
+    GLuint indices[] = {
         0, 1, 3, // first triangle
-        1, 2, 3  // second triangle
+        1, 2, 3 // second triangle
     };
 
     GLuint VBO, VAO, EBO;
@@ -96,11 +92,15 @@ int main(void)
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void *) 0);
     glEnableVertexAttribArray(0);
+
     // tex coords attribute
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void *) (3 * sizeof(GLfloat)));
     glEnableVertexAttribArray(1);
+
+    // unbind the VAO
+    glBindVertexArray(0);
 
     // as we only have a single shader, we could also just activate our shader once beforehand if we want to 
     glUseProgram(shaderProgram);
@@ -109,10 +109,9 @@ int main(void)
     float rotationRadian = 0.0f;
     glm::mat4 cameraMatrix = camera(2.0f);
     GLuint mvpLoc = glGetUniformLocation(shaderProgram, "mvp");
-    
+
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
+    while (!glfwWindowShouldClose(window)) {
         /* Render here */
         glClearColor(0.16f, 0.24f, 0.32f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
@@ -125,10 +124,13 @@ int main(void)
         rotationRadian += 0.01f;
 
         // render the triangle with texture
+        // enable texture unit
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texId);
+
+        // bind VAO
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, nullptr);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
